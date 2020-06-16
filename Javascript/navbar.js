599eb1ec8ba88fbec67d169fe4ca348165446bfa
Kasper <kbla.skp@edu.mercantec.dk>
2020-06-16 20:41:49 +0200

    start up with front page

---
+++ Javascript/navbar.js
@@ -0,0 +1,26 @@
+document.write('<meta charset = "UTF-8">\
+    <link rel="stylesheet" type="text/css" href="../css/forside.css">\
+    <div class="navbar">\
+    <div><a href="../html/forside.html">Forside</a></div>\
+    <div><a href="../html/booking.html">Om klinikken</a></div>\
+    <div><a href="../html/medarbjeder.html">Lidt om mig</a></div>\
+        <div class="dropdown">\
+    <button class="dropbtn">Behandlinger\
+      <i class="fa fa-caret-down"></i>\
+    </button>\
+    <div class="dropdown-content">\
+      <a href="#">Akupunktur</a>\
+      <a href="#">Øreakupunktur</a>\
+      <a href="#">Fodspa</a>\
+      <a href="#">Myorefleksterapi</a>\
+    </div>\
+  </div>\
+    <div><a href="../html/priser.html">Priser</a></div>\
+    <div><a href="../html/booking.html">Booking</a></div>\
+    <div><a href="../html/booking.html">Praktiske oplysninger</a></div>\
+    <div><a href="../html/booking.html">Kontakt</a></div>\
+    <div><a href="../html/booking.html">Tilskud</a></div>\
+    <div><a href="../html/booking.html">Persondatabehandling</a></div>\
+    <div><a href="https://www.facebook.com/Barrith-Akupunktur-805489732821622/">Facebook</a></div>\
+  </div>'
+); 
